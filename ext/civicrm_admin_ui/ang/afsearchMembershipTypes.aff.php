d05614771d3eec3a1a7a290d3ed27ecb3fe7aa2f
Aidan Saunders <aidan.saunders@squiffle.uk>
2023-12-04 18:40:02 +0000

    MembershipType xml - add paths, AdminUI manage MembershipTypes

---
+++ ext/civicrm_admin_ui/ang/afsearchMembershipTypes.aff.php
@@ -0,0 +1,10 @@
+<?php
+use CRM_CivicrmAdminUi_ExtensionUtil as E;
+
+return [
+  'type' => 'search',
+  'title' => E::ts('Membership Types'),
+  'icon' => 'fa-list-alt',
+  'server_route' => 'civicrm/admin/member/membershipType',
+  'modified_date' => '2023-12-04 15:39:42',
+];
